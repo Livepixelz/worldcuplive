0f16bd6d5f61712ef577dfa3ad66762e728a658f
Sylvain Olengh <sylvain.olengha@leslipfrancais.fr>
2018-06-21 09:52:37 +0200

    updated urls to match github.io

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -6,45 +6,56 @@ import Groups from '../groups';
 
 const App = () => (
   <div>
-      <div className="body_background body_background--morning "></div>
-      <div className="body_background body_background--afternoon"></div>
-      <div className="body_background body_background--evening"></div>
-      <div className="body_background body_background--night active"></div>
-      <noscript>
-          You need to enable JavaScript to run this app.
-      </noscript>
-      <div className="wrapper">
-          <header>
-              <div >
-                  <div className="logo"></div>
-                  <h1>LE MONDIAL DU SLIP</h1>
-                  <nav className="nav">
-                      <ul>
-                          <li><Link to="/">Resultats</Link></li>
-                          <li><Link to="/groupes">Groupes</Link></li>
-                          <li><Link to="/equipes">Equipes</Link></li>
-                      </ul>
-                  </nav>
-              </div>
-          </header>
+    <div className="body_background body_background--morning " />
+    <div className="body_background body_background--afternoon" />
+    <div className="body_background body_background--evening" />
+    <div className="body_background body_background--night active" />
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div className="wrapper">
+      <header>
+        <div>
+          <div className="logo" />
+          <h1>LE MONDIAL DU SLIP</h1>
+          <nav className="nav">
+            <ul>
+              <li>
+                <Link to="/ffs/">Resultats</Link>
+              </li>
+              <li>
+                <Link to="/ffs/groupes">Groupes</Link>
+              </li>
+              <li>
+                <Link to="/ffs/equipes">Equipes</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main>
+        <section id="react" className="container">
           <main>
-              <section id="react" className="container">
-                  <main>
-                      <Route exact path="/" component={Home}/>
-                      <Route exact path="/equipes" component={Teams}/>
-                      <Route exact path="/groupes" component={Groups}/>
-                  </main>
-              </section>
+            <Route exact path="/ffs/" component={Home} />
+            <Route exact path="/ffs/equipes" component={Teams} />
+            <Route exact path="/ffs/groupes" component={Groups} />
           </main>
-          <footer className="prodby"> by <span className="glitch" data-text="LIVEPIXELZ">LIVEPIXELZ</span></footer>
+        </section>
+      </main>
+      <footer className="prodby">
+        {' '}
+        by{' '}
+        <span className="glitch" data-text="LIVEPIXELZ">
+          LIVEPIXELZ
+        </span>
+      </footer>
+    </div>
+    <div className="loading__wrapper loaded">
+      <div>
+        <div className="logo" />
+        <p>
+          <span className="loading__text">CHARGEMENT</span>
+        </p>
       </div>
-      <div className="loading__wrapper loaded">
-          <div>
-              <div className="logo"></div>
-              <p><span className="loading__text">CHARGEMENT</span></p>
-          </div>
-      </div>
-
+    </div>
   </div>
 );
 
